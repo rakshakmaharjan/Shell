158279aea68797221975a8edff6d686a105f2434
Rakshak Maharjan <73810433+rakshakmaharjan@users.noreply.github.com>
2024-01-11 11:20:55 +0545

    Add files via upload

---
+++ myscripts/backup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Define source and destination directories.
+source="/home/user/source"
+destination="/home/user/backup"
+
+# Get Date and Time of Backup.
+timestamp=$(date +%Y%m%d_%H%M%S)
+
+# Define backup name.
+backup_name="backup_$timestamp.tar.gz"
+backup="backup_$timestamp.gz"
+
+#tar -zcvf "$destination/$backup_name" -C "$source" . #Backup using TAR.
+
+gzip -r "$source" > "$destination/$backup" #Backup using gzip.
+
+# Check if Backup is Successfull or not.
+if [[ $? -eq 0 ]]
+then
+	echo "Backup Success."
+else
+	echo "Backup Failed."
+fi
